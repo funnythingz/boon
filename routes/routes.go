ea4601712e71a214a57b6aabe69652df790577dd
funnythingz <funnythingz69+github@gmail.com>
2014-08-01 23:34:41 +0900

    added render

--- routes/routes.go
+++ routes/routes.go
@@ -2,17 +2,28 @@ package routes
 
 import (
     "github.com/go-martini/martini"
-    //"github.com/martini-contrib/render"
+    "github.com/martini-contrib/render"
 )
+
+type Header struct {
+    Title string
+    Description string
+}
 
 func Router() {
 
     m := martini.Classic()
 
-    m.Use(martini.Static("assets"))
+    m.Use(render.Renderer(render.Options{
+        Directory: "templates",
+        Layout: "layout",
+        Extensions: []string{".tmpl"},
+        Charset: "utf-8",
+    }))
 
-    m.Get("/", func() string {
-        return "Hello world!"
+    m.Get("/", func(r render.Render) {
+        header := Header{"Index", "Description"}
+        r.HTML(200, "index", header)
     })
 
     m.Get("/ids/:name", func(params martini.Params) string {
