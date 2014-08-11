e6f19ecea5b576d0a6fe03190ad2f48feca97f89
funnythingz <funnythingz69+github@gmail.com>
2014-08-11 23:08:03 +0900

    changed static directory from assets to public

--- boon.go
+++ boon.go
@@ -1,46 +1,45 @@
 package main
 
 import (
-    "net/http"
-    "github.com/codegangsta/negroni"
-    render "github.com/unrolled/render"
-    "log"
+	"github.com/codegangsta/negroni"
+	render "github.com/unrolled/render"
+	"log"
+	"net/http"
 )
 
 func main() {
 
-    r := render.New(render.Options{
-        Directory: "templates",
-        Layout: "layout",
-        Extensions: []string{".tmpl"},
-        Charset: "utf-8",
-    })
+	r := render.New(render.Options{
+		Directory:  "templates",
+		Layout:     "layout",
+		Extensions: []string{".tmpl"},
+		Charset:    "utf-8",
+	})
 
-    mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-    mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-        r.HTML(w, http.StatusOK, "index", nil)
-    })
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		r.HTML(w, http.StatusOK, "index", nil)
+	})
 
-    mux.HandleFunc("/about", func(w http.ResponseWriter, req *http.Request) {
-        r.HTML(w, http.StatusOK, "about", nil)
-    })
+	mux.HandleFunc("/about", func(w http.ResponseWriter, req *http.Request) {
+		r.HTML(w, http.StatusOK, "about", nil)
+	})
 
-    mux.HandleFunc("/new", func(w http.ResponseWriter, req *http.Request) {
-        r.HTML(w, http.StatusOK, "new", map[string]interface{}{
-            "Options": CreateTimeOptions(),
-        })
-    })
+	mux.HandleFunc("/new", func(w http.ResponseWriter, req *http.Request) {
+		r.HTML(w, http.StatusOK, "new", map[string]interface{}{
+			"Options": CreateTimeOptions(),
+		})
+	})
 
-    mux.HandleFunc("/hoge", func(w http.ResponseWriter, req *http.Request) {
-        log.Println(req)
-    })
+	mux.HandleFunc("/hoge", func(w http.ResponseWriter, req *http.Request) {
+		log.Println(req)
+	})
 
-    n := negroni.Classic()
-    n.UseHandler(mux)
-    n.UseHandler(http.NotFoundHandler())
-    n.Use(negroni.NewStatic(http.Dir("./assets")))
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	//n.UseHandler(http.NotFoundHandler())
 
-    n.Run(":3000")
+	n.Run(":3000")
 
 }
